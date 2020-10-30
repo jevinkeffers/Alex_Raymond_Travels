a576e98fefb941ad90a3aef1d4cd69590cc21d90
Kevin Jeffers <jevinkeffers@gmail.com>
2020-10-30 12:27:47 -0400

    Fixed blogs, logo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import logo from '../img/logo.svg'
+import logo from '../img/logo.png'
 import facebook from '../img/social/facebook.svg'
 import instagram from '../img/social/instagram.svg'
 // import twitter from '../img/social/twitter.svg'
@@ -13,8 +13,8 @@ const Footer = class extends React.Component {
         <div className="content has-text-centered">
           <img
             src={logo}
-            alt="Kaldi"
-            style={{ width: '14em', height: '10em' }}
+            alt="Alex Raymond Travels"
+            style={{ width: '35%', height: '35%' }}
           />
         </div>
         <div className="content has-text-centered has-background-black has-text-white-ter">
