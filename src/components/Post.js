f21cf632da5c4679c43b1cebe1ad97ad555357a1
Kevin Jeffers <jevinkeffers@gmail.com>
2020-11-12 16:15:19 -0500

    Added rich text from Contentful

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,16 +1,37 @@
-import React from 'react'
+import React from "react";
+import { BLOCKS, MARKS } from "@contentful/rich-text-types";
+import { documentToHtmlString } from "@contentful/rich-text-html-renderer";
 
-const Post = ({article}) => {
-    console.log(article)
-    const { headline, featuredImage, subhead, location, body } = article.fields
-    // const postBody = marked(body)
+const Post = ({ article }) => {
+    // const options = {
+    //     renderMark: {
+    //         [MARKS.BOLD]: (text) => `<custom-bold>${text}</custom-bold>`
+    //     },
+    //     renderNode: {
+    //         [BLOCKS.PARAGRAPH]: (node, next) =>
+    //             `<custom-paragraph>${next(node.content)}</custom-paragraph>`,
+    //     }
+    // }
+    
+    const { headline, subhead, featuredImage, body } = article.fields;
+
+    const postBody = documentToHtmlString(body);
     return (
-        <div className='post'>
-            <h2 className='title'>{headline}</h2>
-            {featuredImage && <img className='featuredImage' src={featuredImage.fields.file.url} alt={headline} title={headline} />}
-            <section dangerouslySetInnerHTML={{ __html: body  }} />
+        <div className="post">
+            <h1 className="title">{headline}</h1>
+            <h3 className="subhead">{subhead}</h3>
+            {featuredImage && (
+                <img
+                className="featuredImage"
+                src={featuredImage.fields.file.url}
+                alt={headline}
+                title={headline}
+                />
+            )}
+            <section dangerouslySetInnerHTML={{ __html: postBody }} />
+            {/* <section className='location'>{location}</section> */}
         </div>
-    )
-}
+    );
+    };
 
-export default Post 
+export default Post;
